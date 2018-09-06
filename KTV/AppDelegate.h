eb7af7fb64bac040f7adcd0b982390276006332f
sygriffin <786281584@qq.com>
2018-09-06 15:09:01 +0800

    add KTV targety

---
+++ KTV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTV
+//
+//  Created by songyang on 2018/9/6.
+//  Copyright © 2018年 songyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform:ios,'8.0'
+use_frameworks!
+
+target 'KTV' do
+
+pod 'Mantle', '~> 2.1.0'
+
+pod 'AFNetworking', '~> 3.2.1'
+
+pod 'CocoaLumberjack', '~> 3.4.2'
+
+
+end
